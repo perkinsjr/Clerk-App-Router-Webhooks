e16885606b741c40c2228b3b11679c8c064c48e9
James Perkins <jamesperkins@hey.com>
2023-04-21 09:56:54 -0400

    Add Clerk webhooks using new route handlers

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,85 @@
+import type { IncomingHttpHeaders } from "http";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { WebhookRequiredHeaders } from "svix";
+import type { User } from "@clerk/nextjs/api";
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+
+type UnwantedKeys = "emailAddresses" | "firstName" | "lastName" | "primaryEmailAddressId" | "primaryPhoneNumberId" | "phoneNumbers";
+
+interface UserInterface extends Omit<User, UnwantedKeys> {
+  email_addresses: {
+    email_address: string;
+    id: string;
+  }[];
+  primary_email_address_id: string;
+  first_name: string;
+  last_name: string;
+  primary_phone_number_id: string;
+  phone_numbers: {
+    phone_number: string;
+    id: string;
+  }[];
+}
+
+const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
+
+export async function POST(
+  req
+) {
+  const payload = await req.json()
+  const payloadString = JSON.stringify(payload);
+  const headerPayload = headers();
+  const svixId = headerPayload.get("svix-id");
+  const svixIdTimeStamp = headerPayload.get("svix-timestamp");
+  const svixSignature = headerPayload.get("svix-signature");
+  if (!svixId || !svixIdTimeStamp || !svixSignature) {
+    console.log("error")
+    return new Response("Error occured", {
+      status: 400,
+    })
+  }
+  const svixHeaders = {
+    "svix-id": svixId,
+    "svix-timestamp": svixIdTimeStamp,
+    "svix-signature": svixSignature,
+  };
+  const wh = new Webhook(webhookSecret);
+  let evt: Event | null = null;
+  try {
+    evt = wh.verify(payloadString, svixHeaders) as Event;
+  } catch (_) {
+    console.log("error")
+    return new Response("Error occured", {
+      status: 400,
+    })
+  }
+  const { id } = evt.data;
+  // Handle the webhook
+  const eventType: EventType = evt.type;
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { email_addresses, primary_email_address_id } = evt.data;
+    const emailObject = email_addresses?.find((email) => {
+      return email.id === primary_email_address_id;
+    });
+    if (!emailObject) {
+      return new Response("Error locating user", {
+        status: 400,
+      })
+    }
+    console.log("doing something")
+  }
+  console.log(`User ${id} was ${eventType}`);
+  return new Response("", {
+    status: 201,
+  })
+}
+
+
+type Event = {
+  data: UserInterface;
+  object: "event";
+  type: EventType;
+};
+
+type EventType = "user.created" | "user.updated" | "*";
